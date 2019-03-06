3c79cb33622a1fecef6c920b205bfb3a0745d725
Scott Gellock <sgellock@gmail.com>
2019-03-05 19:32:54 -0800

    .NET Templates: update CoreBot to use 4.3
    - update core bot to use the ActivityHandler and MVC pattern
    - use the new FlightBooking LUIS model
    - update PREREQUISITES.md to account for LUIS app import steps
    - update PREREQUISITES.md to document appsettings.json requirements
    - bump the version

---
+++ generators/dotnet-templates/Microsoft.BotFramework.CSharp.CoreBot/content/Dialogs/MainDialog.cs
@@ -0,0 +1,74 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class MainDialog : ComponentDialog
+    {
+        private IConfiguration _configuration;
+
+        public MainDialog(IConfiguration configuration)
+            : base(nameof(MainDialog))
+        {
+            _configuration = configuration;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new BookingDialog());
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                IntroStepAsync,
+                ActStepAsync,
+                FinalStepAsync,
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+        protected IStatePropertyAccessor<BookingDetails> BookingDetailsAccessor { get; set; }
+
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("What can I help you with today?") }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // Call LUIS and gather any potential booking details.
+            var bookingDetails = await LuisHelper.ExecuteLuisQuery(_configuration, stepContext.Context, cancellationToken);
+
+            // Run the BookingDialog giving it whatever details we have from the LUIS call, it will fill out the remainder.
+            return await stepContext.BeginDialogAsync(nameof(BookingDialog), bookingDetails, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // If the child dialog ("BookingDialog") was cancelled or the user failed to confirm, the Result here will be null.
+            if (stepContext.Result != null)
+            {
+                var result = (BookingDetails)stepContext.Result;
+
+                // Now we have all the booking details call the booking service.
+
+                // If the call to the booking service was successful tell the user.
+
+                var timeProperty = new TimexProperty(result.TravelDate);
+                var travelDateMsg = timeProperty.ToNaturalLanguage(DateTime.Now);
+                var msg = $"I have you booked to {result.Destination} from {result.Origin} on {travelDateMsg}";
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
+            }
+            else
+            {
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Thank you."), cancellationToken);
+            }
+            return await stepContext.EndDialogAsync();
+        }
+    }
+}
